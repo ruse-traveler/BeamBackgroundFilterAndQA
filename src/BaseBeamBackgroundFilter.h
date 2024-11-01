bbc4167ffbcb0742dbd843fa490235cd0222f836
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-11-01 16:39:33 -0400

    Add WIP base filter template

---
+++ src/BaseBeamBackgroundFilter.h
@@ -0,0 +1,79 @@
+/// ===========================================================================
+/*! \file    BeamBackgroundFilter.h
+ *  \authors Derek Anderson
+ *  \date    11.01.2024
+ *
+ *  Part of the BeamBackgroundFilterAndQA module, this 
+ *  is an abstract template for any filters run as
+ *  part of the module.
+ */
+/// ===========================================================================
+
+#ifndef BEAMBACKGROUNDFILTER_H
+#define BEAMBACKGROUNDFILTER_H
+
+// c++ utilities
+#include <map>
+#include <string>
+
+// forward declarations
+class PHCompositeNode;
+
+
+
+// ============================================================================
+//! Abstract base filter 
+// ============================================================================
+/*! An abstract template for filters to be applied in the
+ * BeamBackgroundFilterAndQA module
+ */
+template <typename Config> class BaseBeamBackgroundFilter
+{
+
+  protected:
+
+    // ------------------------------------------------------------------------
+    //! Grab relevant input nodes
+    // ------------------------------------------------------------------------
+    /*! This <GRABS THE INPUT>
+     */
+    virtual void GrabNodes(PHCompositeNode* topNode) = 0;
+
+    ///! filter configuration
+    Config m_config;
+
+    /* TODO add histogram map(s) */
+
+  public:
+
+    // ------------------------------------------------------------------------
+    //! Build associated histograms
+    // ------------------------------------------------------------------------
+    /*! This <MAKES THE HISTOGRAMS>
+     */
+    virtual void BuildHistograms(const std::string& tag) = 0;
+
+    // ------------------------------------------------------------------------
+    //! Apply filter
+    // ------------------------------------------------------------------------
+    /*! This <DOES THE THING>
+     */ 
+    virtual void ApplyFilter(PHCompositeNode* topNode) = 0;
+
+    /* TODO add method to register histograms */
+
+    ///! Set filter configuration
+    inline void SetConfig(const Config& config) {m_config = config;}
+
+    ///! Get filter configuration
+    inline Config GetConfig() const {return m_config;}
+
+    ///! default ctor/dtor
+    BaseBeamBackgroundFilter()  {};
+    ~BaseBeamBackgroundFilter() {};
+
+};  // end BaseBeamBackgroundFilter
+
+#endif
+
+// end ========================================================================
